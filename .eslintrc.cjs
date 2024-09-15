545ae4ceab551f69745fd2055f81f95a70231d73
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2024-09-15 10:45:47 +0800

    npm install --save-dev eslint-plugin-prettier eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
 module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    '@electron-toolkit/eslint-config-prettier',
+  ],
+  eslintPluginPrettierRecommended,
+};
